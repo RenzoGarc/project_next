4fbf1d4fab717a788d52f6ac3a04b7d91ab52ec4
RENZO GARCIA AUQUI <rgarcia@ideasg.org>
2023-08-18 10:47:35 -0500

    probando

--- app/landing/page.jsx
+++ app/landing/page.jsx
@@ -1,21 +1,36 @@
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic";
 
-const Banner = dynamic(() => import('@components/landing/Banner/index'), { ssr: false })
-const Companies = dynamic(() => import('@components/landing/Companies/Companies'), { ssr: false })
-const Courses = dynamic(() => import('@components/landing/Courses/index'), { ssr: false })
-const Mentor = dynamic(() => import('@components/landing/Mentor/index'), { ssr: false })
-const Testimonials = dynamic(() => import('@components/landing/Testimonials/index'), { ssr: false })
-const Newsletter = dynamic(() => import('@components/landing/Newsletter/Newsletter'), { ssr: false })
+const Banner = dynamic(() => import("@components/landing/Banner/index"), {
+  ssr: false,
+});
+const Companies = dynamic(
+  () => import("@components/landing/Companies/Companies"),
+  { ssr: false }
+);
+const Courses = dynamic(() => import("@components/landing/Courses/index"), {
+  ssr: false,
+});
+const Mentor = dynamic(() => import("@components/landing/Mentor/index"), {
+  ssr: false,
+});
+const Testimonials = dynamic(
+  () => import("@components/landing/Testimonials/index"),
+  { ssr: false }
+);
+const Newsletter = dynamic(
+  () => import("@components/landing/Newsletter/Newsletter"),
+  { ssr: false }
+);
 
-export default function LandingPage () {
+export default function LandingPage() {
   return (
     <div>
       <Banner />
       <Companies />
       <Courses />
       <Mentor />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Newsletter />
     </div>
   );
-};
+}
